232c23c3bb012afdd2fdbf27b1de46ec54bd8102
chriswkingg <christopherking@rogers.com>
2021-11-22 14:04:59 -0500

    Draw some bars

--- Main.cpp
+++ Main.cpp
@@ -1,23 +1,39 @@
 #include <SFML/Graphics.hpp>
+#include <random>
+#include <iostream>
+#include <thread>
+#include <chrono>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(400, 600), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+#define SCREENWIDTH 600
+#define SCREENHEIGHT 400
 
+using namespace sf;
+using namespace std::chrono_literals; //this is probably bad
+
+void DrawBars(RenderWindow &window, int *barHeight) {
+    for(int i = 0; i < 10; i++) {
+        RectangleShape bar(Vector2f(100, barHeight[i]));
+        bar.setPosition(SCREENWIDTH / 10 * i, SCREENHEIGHT - barHeight[i]);
+        window.draw(bar);
+    }
+}
+
+int main() {
+    RenderWindow window(VideoMode(SCREENWIDTH, SCREENHEIGHT), "Bubble Sort");
+    int barHeight[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     while (window.isOpen())
     {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
-
         window.clear();
-        window.draw(shape);
+        DrawBars(window, barHeight);
         window.display();
+        std::this_thread::sleep_for(2000ms);
+        barHeight[0] = 1000;
     }
 
     return 0;
